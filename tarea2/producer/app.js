513303f0f212e9aac91c97ba1f2c185a52fe134f
BenjaminT <benjamin.torres1@mail.udp.cl>
2022-10-30 16:26:51 -0300

    working producer

--- tarea2/producer/app.js
+++ tarea2/producer/app.js
@@ -1,29 +1,91 @@
 //basic api with kafka
 const express = require("express");
+const bodyParser = require("body-parser");
 const { Kafka } = require("kafkajs");
-const app = express();
-const port = 3000;
+const { Partitioners } = require('kafkajs')
+
+//const dotenv = require("dotenv");
+//const port = 3000;
 const kafka = new Kafka({
   clientId: "my-app",
-  brokers: ["localhost:9092"],
+  brokers: ["kafka:9092"],
 });
+
+kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner })
+const producer = kafka.producer()
+
+const admin = kafka.admin()
+
+    
+admin.connect()
+    // await producer.connect()
+admin.createTopics({
+      waitForLeaders: true,
+      topics: [
+        { topic: 'members',
+          numPartitions: 2, 
+          replicationFactor: 1, 
+      },
+      ],
+})
+
+
+// const admin = kafka.admin()
+// await admin.connect()
+
+// await admin.listTopics()
+
+// const membersTopic ={
+//   topic: 'members',
+//   numPartitions: 2,     // default: -1 (uses broker `num.partitions` configuration)
+//   replicationFactor: 3, // default: -1 (uses broker `default.replication.factor` configuration)
+// }
+
+
+
+
+// await admin.createTopics({
+//   validateOnly: false,
+//   waitForLeaders: true,
+//   topics: membersTopic,
+// })
+
+
+//server.server();
+const app = express();
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// );
+app.use(bodyParser.json());
+//app.use(cors());
+
+var port = process.env.PORT || 3000;
+var host = process.env.PORT || '0.0.0.0';
+///////////////////////////////////////////////////////////////
+
 
 app.post("/submit", (req, res) => {
   console.log("submit");
   (async () => {
-    const producer = kafka.producer();
+    console.log("hola");
     await producer.connect();
-    if (req.body.premium == true) {
+    console.log("hola2");
+    const { name, premium} = req.body;
+    if (premium == true) {
       await producer.send({
         topic: "members",
-        messages: req.body,
-        partition: 1,
+        messages: [
+          { key: 'name', value: "premium", partition: 1 },
+        ],
       });
     } else {
       await producer.send({
         topic: "members",
-        messages: req.body,
-        partition: 0,
+        messages: [
+          { key: 'name', value: "non-premium", partition: 0 },
+        ],
       });
     }
     await producer.disconnect();
@@ -51,12 +113,12 @@ app.post("/ventas", (req, res) => {
 
     await producer.send({
       topic: "location",
-      messages: location,
+      messages: [{ value: JSON.stringify(location)}],
       partition: 1,
     });
     await producer.send({
       topic: "stock",
-      messages: stock,
+      messages: [{ value: JSON.stringify(stock)}],
       partition: 1,
     });
 
